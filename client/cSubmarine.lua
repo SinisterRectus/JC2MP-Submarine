b3854ef4b824715ccd9ad743d659217161e7a0b2
SinisterRectus <sinister.rectus@gmail.com>
2016-09-27 17:09:39 -0400

    Code cleanup and optimizations

--- client/cSubmarine.lua
+++ client/cSubmarine.lua
@@ -1,24 +1,17 @@
+local clamp, ceil, abs = math.clamp, math.ceil, math.abs
+
 class 'Submarine'
 
 function Submarine:__init()
-	
+
 	self.altitude_step = 0.2 -- 0.2
 	self.sea_level = 199.6 -- 199.6
-	
+
 	self.altitude_gain = 20 -- 20
-	
+
 	self.pitch_gain = 25 -- 25
 	self.yaw_gain = 0.6 -- 0.6
 	self.roll_gain = 1 -- 1
-
-	if LocalPlayer:InVehicle() then
-		local vehicle = LocalPlayer:GetVehicle()
-		if vehicle:GetModelId() == 88 then
-			self.vehicle = vehicle
-			self.altitude = self.sea_level
-			self.sub = Events:Subscribe("PreTick", self, self.Control)
-		end
-	end
 
 	Events:Subscribe("LocalPlayerEnterVehicle", self, self.Enter)
 	Events:Subscribe("LocalPlayerExitVehicle", self, self.Exit)
@@ -26,138 +19,124 @@ function Submarine:__init()
 	Events:Subscribe("EntityDespawn", self, self.Despawn)
 	Events:Subscribe("ModulesLoad", self, self.AddHelp)
 	Events:Subscribe("ModuleUnload", self, self.RemoveHelp)
-	
+
 end
 
 function Submarine:BlockEject()
-
-	if IsValid(self.vehicle) then
-		return false
-	end
-
+	return not IsValid(self.vehicle)
 end
 
 function Submarine:Control(args)
 
-	if not IsValid(self.vehicle) then return end
-	
-	local velocity = self.vehicle:GetLinearVelocity()
-	local position = self.vehicle:GetPosition()
-	local angle = self.vehicle:GetAngle()
+	local vehicle = self.vehicle
+	if not IsValid(vehicle) then return end
+
+	local velocity = vehicle:GetLinearVelocity()
+	local position = vehicle:GetPosition()
+	local angle = vehicle:GetAngle()
 	local speed = -(-angle * velocity).z
 
+	local altitude = self.altitude
+	local sea_level = self.sea_level
+
 	if Input:GetValue(Action.HeliIncAltitude) > 0 then
-		self.altitude = self.altitude + self.altitude_step
+		altitude = altitude + self.altitude_step
 	elseif Input:GetValue(Action.HeliDecAltitude) > 0 then
-		self.altitude = self.altitude - self.altitude_step
+		altitude = altitude - self.altitude_step
 	end
-	
-	self.altitude = math.clamp(self.altitude, math.ceil(Physics:GetTerrainHeight(position)), self.sea_level)
-	
-	local accel, yaw
-	
+
+	altitude = clamp(altitude, ceil(Physics:GetTerrainHeight(position)), self.sea_level)
+	self.altitude = altitude
+
+	local accel = 0
+
 	if Input:GetValue(Action.Accelerate) > 0 then
-		accel = self.vehicle:GetValue("max_accel") - (self.vehicle:GetValue("max_accel") / self.vehicle:GetValue("max_speed")^2) * speed^2
+		local max_accel = vehicle:GetValue("max_accel")
+		local max_speed = vehicle:GetValue("max_speed")
+		accel = max_accel - (max_accel / max_speed^2) * speed^2
 	elseif Input:GetValue(Action.Reverse) > 0 then
-		accel = -0.1 * (self.vehicle:GetValue("max_accel") - (self.vehicle:GetValue("max_accel") / self.vehicle:GetValue("max_speed")^2) * speed^2)
-	else	
-		accel = 0
+		local max_accel = vehicle:GetValue("max_accel")
+		local max_speed = vehicle:GetValue("max_speed")
+		accel = -0.1 * (max_accel - (max_accel / max_speed^2) * speed^2)
 	end
-	
+
+	local yaw = 0
 	if Input:GetValue(Action.TurnLeft) > 0 then
 		yaw = 1
 	elseif Input:GetValue(Action.TurnRight) > 0 then
 		yaw = -1
-	else
-		yaw = 0
 	end
 
-	if self.altitude < self.sea_level or position.y < self.sea_level then
-	
-		self.vehicle:SetLinearVelocity(velocity + angle * Vector3(0, self.altitude_gain * (self.altitude - position.y), -accel) * args.delta)
-	
-		self.vehicle:SetAngularVelocity(angle * Vector3(self.pitch_gain * -angle.pitch, math.abs(speed)/speed * self.yaw_gain * yaw, -self.roll_gain * angle.roll))
-	
+	if altitude < sea_level or position.y < sea_level then
+
+		local yaw_gain = self.yaw_gain
+		local roll_gain = self.roll_gain
+		local pitch_gain = self.pitch_gain
+		local altitude_gain = self.altitude_gain
+
+		vehicle:SetLinearVelocity(velocity + angle * Vector3(0, altitude_gain * (altitude - position.y), -accel) * args.delta)
+		vehicle:SetAngularVelocity(angle * Vector3(pitch_gain * -angle.pitch, abs(speed)/speed * yaw_gain * yaw, -roll_gain * angle.roll))
 		LocalPlayer:SetOxygen(1)
-		
-		-- Chat:Print("submerged", Color.Red)
-		
-	elseif position.y < self.sea_level + 1 then
-	
-		self.vehicle:SetLinearVelocity(velocity + angle * Vector3(0, 0, -accel) * args.delta)
-		
-		self.vehicle:SetAngularVelocity(angle * Vector3(self.vehicle:GetAngularVelocity().x, math.abs(speed)/speed * self.yaw_gain * yaw, self.vehicle:GetAngularVelocity().z))
-		
-		-- Chat:Print("on water", Color.Red)
-		
+
+		-- Chat:Print("submerged", Color.Red) -- debug
+
+	elseif position.y < sea_level + 1 then
+
+		vehicle:SetLinearVelocity(velocity + angle * Vector3(0, 0, -accel) * args.delta)
+		vehicle:SetAngularVelocity(angle * Vector3(vehicle:GetAngularVelocity().x, abs(speed)/speed * self.yaw_gain * yaw, vehicle:GetAngularVelocity().z))
+
+		-- Chat:Print("on water", Color.Red) -- debug
+
 	-- else
-	
-		-- Chat:Print("above water", Color.Red)
-		
+
+		-- Chat:Print("above water", Color.Red) -- debug
+
 	end
-	
-	-- Chat:Print(tostring(self.altitude), Color.Red)
 
 end
 
 function Submarine:Enter(args)
-
 	if args.vehicle:GetModelId() == 88 and args.vehicle:GetValue("max_speed") and args.vehicle:GetValue("max_accel") then
 		self.vehicle = args.vehicle
 		self.altitude = self.sea_level
-		if not self.sub then
-			self.sub = Events:Subscribe("PreTick", self, self.Control)
-		end
+		self.sub = self.sub or Events:Subscribe("PreTick", self, self.Control)
 	end
-
 end
 
 function Submarine:Exit(args)
-
-	if self.vehicle then
-		self.vehicle = nil
-		if self.sub then
-			Events:Unsubscribe(self.sub)
-			self.sub = nil
-		end
-	end
-
+	if self.vehicle then self:Reset() end
 end
 
 function Submarine:Despawn(args)
+	if args.entity == self.vehicle then self:Reset() end
+end
 
-	if args.entity.__type == "Vehicle" and args.entity == self.vehicle then
-		self.vehicle = nil
-		if self.sub then
-			Events:Unsubscribe(self.sub)
-			self.sub = nil
-		end
+function Submarine:Reset()
+	self.vehicle = nil
+	if self.sub then
+		Events:Unsubscribe(self.sub)
+		self.sub = nil
 	end
-		
 end
 
 function Submarine:AddHelp()
-
     Events:Fire("HelpAddItem",
         {
             name = "Submarine",
-            text = 
-                "This allows you to spawn a submersible MTA Powerrun 77." .. 
+            text =
+                "This allows you to spawn a submersible MTA Powerrun 77." ..
 				"\n\nEnter /sub [max accel] [max speed] to spawn a submarine," ..
                 "\nwhere max accel and max speed are numbers. If these are" ..
 				"\nomited, the defaults are 5 and 35 m/s, respectively." ..
 				"\n\nPress Ctrl to dive and Shift to surface."
         })
-
 end
 
 function Submarine:RemoveHelp()
-
     Events:Fire("HelpRemoveItem",
         {
             name = "Submarine"
         })
-
 end
 
 Submarine = Submarine()
--- server/sSubmarine.lua
+++ server/sSubmarine.lua
@@ -1,30 +1,32 @@
+local max = math.max
+
 class 'Submarine'
 
 function Submarine:__init()
-
 	self.vehicles = {}
 	Events:Subscribe("PlayerChat", self, self.OnPlayerChat)
 	Events:Subscribe("PlayerQuit", self, self.OnPlayerQuit)
 	Events:Subscribe("ModuleUnload", self, self.OnModuleUnload)
-	
 end
 
 function Submarine:Create(args, max_accel, max_speed, creator)
 
+	local vehicles = self.vehicles
+
 	local id = creator:GetId()
-	if IsValid(self.vehicles[id]) then
-		self.vehicles[id]:Remove()
-		self.vehicles[id] = nil
+	if IsValid(vehicles[id]) then
+		vehicles[id]:Remove()
+		vehicles[id] = nil
 	end
 
 	local vehicle = Vehicle.Create(args)
-	
-	vehicle:SetNetworkValue("max_accel", math.max(max_accel or 5, 1))
-	vehicle:SetNetworkValue("max_speed", math.max(max_speed or 35, 5))
+
+	vehicle:SetNetworkValue("max_accel", max(max_accel or 5, 1))
+	vehicle:SetNetworkValue("max_speed", max(max_speed or 35, 5))
 	-- May not always match actual max speed, depending upon acceleration and drag
 
-	self.vehicles[id] = vehicle
-	
+	vehicles[id] = vehicle
+
 	return vehicle
 
 end
@@ -32,39 +34,31 @@ end
 function Submarine:OnPlayerChat(args)
 
 	local text = args.text:split(" ")
-	
+
 	if text[1] == "/sub" then
-		
 		spawn_args = {
 			model_id = 88,
 			position = args.player:GetPosition(),
 			angle = args.player:GetAngle(),
 		}
-		
 		args.player:EnterVehicle(self:Create(spawn_args, tonumber(text[2]), tonumber(text[3]), args.player), 0)
-		
 		return false
-		
 	end
 
 end
 
 function Submarine:OnPlayerQuit(args)
-
 	local id = args.player:GetId()
 	if IsValid(self.vehicles[id]) then
 		self.vehicles[id]:Remove()
 		self.vehicles[id] = nil
 	end
-
 end
 
 function Submarine:OnModuleUnload()
-
 	for _, vehicle in pairs(self.vehicles) do
 		if IsValid(vehicle) then vehicle:Remove() end
 	end
-
 end
 
 Submarine = Submarine()
